a0eee1dea817a4cb48508d19a9c1905235e0f3e0
Emanuel Farauanu <rototuro@gmail.com>
2020-07-06 16:09:42 +0300

    working initial db connection and collection finding test

--- backend/mongoclient/mongoclient_test.go
+++ backend/mongoclient/mongoclient_test.go
@@ -6,9 +6,27 @@ import (
 	"github.com/Rototu/StoreFront/mongoclient"
 )
 
-func TestDBConnection(t *testing.T) {
+func TestDB(t *testing.T) {
 	cl, err := mongoclient.Connect()
 	if err != nil {
 		t.Errorf("Error encounterd in connecting to DB: %s", err)
 	}
+	t.Run("Getting \"Items\" collection", func(t *testing.T) {
+		_, err := mongoclient.GetCollection(cl, mongoclient.Items)
+		if err != nil {
+			t.Errorf("\nError encountered in getting %s collection: %s", mongoclient.Items, err.Error())
+		}
+	})
+	t.Run("Getting \"Users\" collection", func(t *testing.T) {
+		_, err := mongoclient.GetCollection(cl, mongoclient.Ratings)
+		if err != nil {
+			t.Errorf("\nError encountered in getting %s collection: %s", mongoclient.Ratings, err.Error())
+		}
+	})
+	t.Run("Getting \"Ratings\" collection", func(t *testing.T) {
+		_, err := mongoclient.GetCollection(cl, mongoclient.Users)
+		if err != nil {
+			t.Errorf("\nError encountered in getting %s collection: %s", mongoclient.Users, err.Error())
+		}
+	})
 }
