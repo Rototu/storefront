8083e113de1815df3c67d384fe0f9974aeae4a4c
Emanuel Farauanu <rototuro@gmail.com>
2020-07-06 22:15:22 +0300

    added user insertion method and simplified error handling

--- backend/mongoclient/db.go
+++ backend/mongoclient/db.go
@@ -2,8 +2,6 @@ package mongoclient
 
 import (
 	"context"
-	"errors"
-	"log"
 
 	"github.com/Rototu/StoreFront/env"
 
@@ -18,41 +16,69 @@ func Connect() (*mongo.Client, error) {
 	defer cancel()
 
 	// get mongo url from env
-	environment := env.MakeEnv()
+	environment, err := env.MakeEnv()
+	if err != nil {
+		return nil, err
+	}
 	mongourl, err := environment.LookUp("MONGO_URL")
 	if err != nil {
-		log.Fatal(err)
-		return nil, errors.New("Missing DB env vars")
+		return nil, err
 	}
 
 	// init connection
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongourl))
 	if err != nil {
-		log.Fatal(err)
-		return nil, errors.New("DB Connection failed")
+		return nil, err
 	}
-
-	log.Println("Connected to DB")
 
 	return client, nil
 }
 
 // Disconnect disconnects from mongodb
-func Disconnect(client *mongo.Client) {
+func Disconnect(client *mongo.Client) error {
 	if err := client.Disconnect(context.Background()); err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
 
 // GetCollection gets mongodb collection
 func GetCollection(client *mongo.Client, collectionName Collection) (*mongo.Collection, error) {
-	environment := env.MakeEnv()
+	environment, err := env.MakeEnv()
+	if err != nil {
+		return nil, err
+	}
 	dbName, err := environment.LookUp("DB_NAME")
 	if err != nil {
-		return nil, errors.New("Could not obtain db name fron env")
+		return nil, err
 	}
 
 	collection := client.Database(dbName).Collection(collectionName.String())
 
 	return collection, nil
+}
+
+func prepareUsersForInsertion(users []*User) []interface{} {
+	insertions := make([]interface{}, len(users))
+	for _, userPt := range users {
+		insertions = append(insertions, *userPt)
+	}
+	return insertions
+}
+
+// InsertUsers inserts user in db
+func InsertUsers(userCollection *mongo.Collection, users []*User) (*InsertManyResult, error) {
+
+	// create temp ctx
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// prepare users for insertion
+	insertions := prepareUsersForInsertion(users)
+
+	// insert
+	insertResult, err := userCollection.InsertMany(ctx, insertions)
+
+	return insertResult, err
 }
