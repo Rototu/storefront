e874731d5470780ecc7fae66ce936ee83ee6a7e2
Emanuel Farauanu <rototuro@gmail.com>
2020-07-06 16:10:43 +0300

    removed src folder

--- build.bat
+++
@@ -1,5 +0,0 @@
-cd "frontend\StoreHTML"
-dotnet publish
-cd "../../"
-robocopy frontend\\StoreHTML\\src\\StoreHTML.Client\\bin\\Debug\\netstandard2.1\\publish\\wwwroot static * /mir
-go build -o bin\StoreServer_win.exe
