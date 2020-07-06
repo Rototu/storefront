e616413cb3e0c10dbea3ca45f7843b32e1d66348
Emanuel Farauanu <rototuro@gmail.com>
2020-07-06 15:06:20 +0300

    changed commands in build.sh to accomodate directory restructuring

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# build.sh
+
 blue=$(tput setaf 4)
 normal=$(tput sgr0)
 
@@ -8,9 +10,12 @@ dotnet publish
 cd ../../
 printf "%s\n" "${blue}Copying html output to static folder...${normal}"
 cp -r frontend/StoreHTML/src/StoreHTML.Client/bin/Debug/netstandard2.1/publish/wwwroot/* static/
+printf "%s\n" "${blue}Testing Go Project...${normal}"
+cd backend
+go test -v ./...
 printf "%s\n" "${blue}Building Go Project...${normal}"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    go build -o bin/StoreServer_linux.a
+    go build -o ../bin/StoreServer_linux.a
 elif [[ "$OSTYPE" == "msys" ]]; then
-    go build -o bin/StoreServer_win.exe
+    go build -o ../bin/StoreServer_win.exe
 fi
